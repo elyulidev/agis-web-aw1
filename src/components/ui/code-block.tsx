2c84f56fb261b324eb39446bba630d545b556ee2
elyulidev <elyuli.dev@gmail.com>
2025-09-18 13:48:18 +0300

    update code-block.tsx

--- src/components/ui/code-block.tsx
+++ src/components/ui/code-block.tsx
@@ -7,42 +7,6 @@ import {
 	oneLight,
 	oneDark,
 } from "react-syntax-highlighter/dist/esm/styles/prism";
-
-/* const CopyIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-	<svg
-		{...props}
-		xmlns='http://www.w3.org/2000/svg'
-		width='24'
-		height='24'
-		viewBox='0 0 24'
-		fill='none'
-		stroke='currentColor'
-		strokeWidth='2'
-		strokeLinecap='round'
-		strokeLinejoin='round'
-	>
-		<rect width='14' height='14' x='8' y='8' rx='2' ry='2' />
-		<path d='M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2' />
-	</svg>
-);
-
-const CheckIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-	<svg
-		{...props}
-		xmlns='http://www.w3.org/2000/svg'
-		width='24'
-		height='24'
-		viewBox='0 0 24'
-		fill='none'
-		stroke='currentColor'
-		strokeWidth='2'
-		strokeLinecap='round'
-		strokeLinejoin='round'
-	>
-		<path d='M20 6 9 17l-5-5' />
-	</svg>
-); */
-
 interface CodeBlockProps {
 	codeString: string;
 	language: string;
@@ -92,9 +56,16 @@ export default function CodeBlock({ codeString, language }: CodeBlockProps) {
 				customStyle={{
 					margin: 0,
 					padding: "1.25rem",
-					backgroundColor: "transparent",
+					borderBottomLeftRadius: "0.5rem",
+					borderBottomRightRadius: "0.5rem",
 				}}
-				codeTagProps={{ style: { fontFamily: "monospace" } }}
+				codeTagProps={{
+					style: {
+						fontFamily: "'Fira Code', monospace",
+						fontSize: "14px",
+					},
+				}}
+				showLineNumbers
 			>
 				{codeString.trim()}
 			</SyntaxHighlighter>
